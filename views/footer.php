a1520883d76ea80777b37a38c2e896f91b269245
unknown <2017.jayesh.samtani@ves.ac.in>
2019-11-07 15:09:41 +0530

    second commit

--- views/footer.php
+++ views/footer.php
@@ -2,13 +2,13 @@
 
     <div class="container">
         
-        <p>&copy; My Website 2016</p>
+        <p>&copy; My Website 2019</p>
     
     </div>
 
 </footer>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
 
 <!-- Modal -->
